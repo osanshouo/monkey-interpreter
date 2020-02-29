99087a5ae53cfc40b089ecf12ac0ec31623035b2
osanshouo <53166653+osanshouo@users.noreply.github.com>
2020-02-29 15:31:45 +0900

    add fn crate::evaluate

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,13 @@ pub mod env;
 pub mod eval;
 pub mod repl;
 pub mod error;
+
+
+pub fn evaluate(input: &str) -> Result<crate::object::Object, crate::error::MonkeyError> {
+    let mut env = eval::Evaluator::new();
+    let l = lexer::Lexer::new(input);
+    let mut p = parser::Parser::new(l);
+    let program = p.parse_program()?;
+
+    env.eval(&program)
+}
